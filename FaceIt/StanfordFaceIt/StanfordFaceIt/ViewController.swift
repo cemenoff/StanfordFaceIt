c8aa72817534121a1af7513f20138f2afe60fa0a
Anton Semenov <cemenoff@gmail.com>
2017-10-03 18:06:14 +0300

    StanfordLesson4
    Stanford Lesson 4 FaceIt

---
+++ FaceIt/StanfordFaceIt/StanfordFaceIt/ViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ViewController.swift
+//  StanfordFaceIt
+//
+//  Created by Anton Semenov on 03/10/2017.
+//  Copyright © 2017 Anton Semenov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+
+}
+
